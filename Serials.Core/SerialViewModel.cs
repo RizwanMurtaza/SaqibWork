739d9300a2095168097b80387f1cd46eeadafbfd
Rizwan <rizwan.murtaza@sage.com>
2021-02-14 20:08:39 +0000

    missing auth

--- Serials.Core/SerialViewModel.cs
+++ Serials.Core/SerialViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Serials.Core
 {
@@ -10,12 +11,14 @@ namespace Serials.Core
 
         public string Lastname { get; set; }
 
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public string ActivationDate { get; set; }
 
         public string Email { get; set; }
 
         public string Info { get; set; }
 
+        [Required(ErrorMessage = "Please Enter Serial Number")]
         public string SerialNumber { get; set; }
 
         public string Software { get; set; }
@@ -23,9 +26,10 @@ namespace Serials.Core
 
         public static SerialViewModel ToViewModel(Serials model)
         {
+           
             return new SerialViewModel
             {
-                ActivationDate = model.ActivationDate.ToString(),
+                ActivationDate = model.ActivationDate.UnixTimeStampToDateTime().ToShortDateString(),
                 Email = model.Email,
                 Firstname = model.Firstname,
                 Lastname = model.Lastname,
@@ -36,15 +40,16 @@ namespace Serials.Core
         }
         public static Serials ToEntity(SerialViewModel model)
         {
+            ExtensionMethods.TryConvert(int.Parse(model.Software), out SoftwareEnum software);
             return new Serials
             {
-                ActivationDate = DateTime.Parse(model.ActivationDate).Ticks,
+                ActivationDate = DateTime.Parse(model.ActivationDate).ToUnixTime(),
                 Email = model.Email,
                 Firstname = model.Firstname,
                 Lastname = model.Lastname,
                 Info = model.Info,
                 SerialNumber = model.SerialNumber,
-                Software = model.Software
+                Software = software.ToString()
             };
         }
     }
